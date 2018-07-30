759c9dbcc04787a6344e3d8a3ab3171aad6a9c20
rudreshsolanki97 <rudreshsolanki101@gmail.com>
2018-07-30 22:19:53 +0530

    basic layout & video rendering

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from './components/video_detail'
+const API_KEY = "AIzaSyDcAqJ17sO_H38w28t1VHe4QmS8IbEGbfA";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+    YTSearch({ key: API_KEY, term: "surfboards" }, videos => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector(".container"));
